e7a6929536e0f9f8dc86da72a935dc3b16dadab6
jeonghoonji <wlwjdgns0920@naver.com>
2023-11-29 22:10:21 +0900

    초기 프로젝트 세팅

---
+++ Mongmori/Mongmori/MongmoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  MongmoriApp.swift
+//  Mongmori
+//
+//  Created by 지정훈 on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct MongmoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
